7a2662bc7ed85c43ec8a52121fd2eeaa88d519f4
Axel <aalmontecuunsa.edu.pe>
2024-11-04 09:31:55 -0800

    Initial Commit

---
+++ Lab_06/Lab_06App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_06App.swift
+//  Lab_06
+//
+//  Created by Axel Almonte on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab_06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
